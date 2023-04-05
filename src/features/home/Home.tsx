20eb258385e94f13af54bea79431dc801303ebc7
alinakornieieva <alinakorneeva401@gmail.com>
2023-04-05 15:52:03 +0300

    improve search and improve styling on home page

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -4,19 +4,26 @@ import './Home.scss'
 import useDebounce from "../../hook/debounce"
 
 const Home = () => {
-    const {isError, isLoading, data} = useSearchUsersQuery('alina')
     const [search, setSearch] = useState('')
     const debounce = useDebounce(search)
+    const [dropdown, setDropdown] = useState(false)
+    const {isError, isLoading, data} = useSearchUsersQuery(debounce, {
+        skip: debounce.length < 1
+    })
     useEffect(() => {
         console.log(debounce)
-    }, [debounce])
+        setDropdown(debounce.length > 0 && data?.length! > 0)
+    }, [debounce, data])
     return <main>
         <div className="input-div">
             <input type="text" placeholder="Search for Github user..."
             value={search} onChange={(e) => setSearch(e.currentTarget.value)}/>
-            <div>
-                Lorem ipsum Reiciendis vero laboriosam rem suscipit 
-            </div>
+            {dropdown && <ul className={data?.length! > 8 ? 'scroll' : ''}>
+                {isLoading && <p>Loading...</p>}
+                {data?.map(user => <li key={user.id}>
+                    {user.login}
+                </li>)}
+            </ul>}
         </div>
     </main>
 }
