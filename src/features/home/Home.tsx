f5c0d353de49591fc5b2809bce1141d1df7e3f19
alinakornieieva <alinakorneeva401@gmail.com>
2023-04-05 18:48:32 +0300

    update Home.tsx

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -11,8 +11,9 @@ const Home = () => {
     const {isError, isLoading, data} = useSearchUsersQuery(debounce, {
         skip: debounce.length < 1
     })
-    const [fetchRepos, {data: repos, isLoading: areREposLoading, isError: areReposError}] = useLazyGetUserQuery()
+    const [fetchRepos, {data: repos, isLoading: areReposLoading, isError: areReposError}] = useLazyGetUserQuery()
     const onUserClick = (userName: string) => {
+        setDropdown(false)
         fetchRepos(userName)
     }
     useEffect(() => {
@@ -23,12 +24,15 @@ const Home = () => {
         value={search} onChange={(e) => setSearch(e.currentTarget.value)}/>
         {dropdown && <ul className={data?.length! > 8 ? 'scroll' : ''}>
             {isLoading && <p>Loading...</p>}
+            {isError && <p>Something went wrong...</p>}
             {data?.map(user => <li key={user.id}
             onClick={() => onUserClick(user.login)}>
                 {user.login}
             </li>)}
         </ul>}
-        {repos?.map((repo) => <Repo key={repo.url} repo={repo}/>)}
+        {areReposLoading && <p>Repos are loading...</p>}
+        {areReposError && <p>Something went wrong...</p>}
+        {dropdown || debounce.length === 0 ? null : repos?.map((repo) => <Repo key={repo.url} repo={repo}/>)}
     </main>
 }
 
